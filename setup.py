41e894ef998a1c8a85053158e9bba13eae85c449
yishai <yishairasowsky@gmail.com>
2020-12-06 19:27:03 +0200

    change name and email in setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'scrapeasy',
-  packages = ['scrapeasy'],
+  name = 'scrapeasy_copy',
+  packages = ['scrapeasy_copy'],
   version = '0.12',
   license='MIT',
   description = 'Scraping in python made easy - receive the content you like in just one line of code!',
-  author = 'Joel Barmettler',
-  author_email = 'joel.barmettler@uzh.ch',
-  url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
+  author = 'disciple of Joel Barmettler',
+  author_email = 'yishairasowsky@gmail.com',
+  url = 'https://github.com/yishairasowsky/scrapeasy',
   download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_3.tar.gz',
   keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
   install_requires=[
